4515eb96a7cb00b697c639a2f549cfe7653671c6
alireza sharifi <a.sharifi@wcoe.eu>
2023-07-26 14:13:07 +0330

    setup.py ready for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
-# this python file uses the following encoding utf-8
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+project_urls = {
+  'Github': 'https://github.com/adamcupial/lighthouse-python',
+}
 
 setup(
-    name='lighthouse',
-    version='1.0.2',
-    description='Lightouse runner',
+    name='lighthouse_python',
+    version='1.0.3',
+    description='Lighthouse runner is a Python package that wraps the Lighthouse tool for easy integration into Python projects.',
     author='Adam Cupial',
     author_email='cupial.adam@gmail.com',
-    url='n/a',
+    project_urls=project_urls,
     packages=find_packages(),
     install_requires=[
         'tqdm>=4.30',
-    ]
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
